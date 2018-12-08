e6d1ad064043128d400db0a620b8ed3b4dafbbb5
maxifjaved <axif.javed@gmail.com>
2018-12-09 01:23:01 +0500

    added auth file

---
+++ src/mailer/index.js
@@ -0,0 +1,44 @@
+import nodemailer from "nodemailer";
+
+const from = '"Backend Api" <axif.javed@gmail.com>';
+
+function setup() {
+    return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
+
+export function sendConfirmationEmail(user) {
+    const tranport = setup();
+    const email = {
+        from,
+        to: user.email,
+        subject: "Welcome to Bookworm",
+        text: `
+    Welcome. Please, confirm your email.
+    ${user.generateConfirmationUrl()}
+    `
+    };
+
+    tranport.sendMail(email);
+}
+
+export function sendResetPasswordEmail(user) {
+    const tranport = setup();
+    const email = {
+        from,
+        to: user.email,
+        subject: "Reset Password",
+        text: `
+    To reset password follow this link
+    ${user.generateResetPasswordLink()}
+    `
+    };
+
+    tranport.sendMail(email);
+}
