5e2d7fa40e26f3dfe1cf824207196979929036cf
Ajlal Khan <jalalkhan11223@gmail.com>
2019-03-01 12:12:43 +0500

    html added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -123,19 +123,6 @@ router.get('/confirm-email/:token', async (req, res) => {
     }
 });
 
-router.get('/send-reset-password-email/:identifier', async (req, res) => {
-    const { identifier } = req.params;
-
-    try {
-        let user = await getUserByIdentifier(identifier);
-        if (!user) { return res.status(404).json({ errors: { message: 'User with such email/username doesn\'t exist in system.' } }); }
-        await sendResetPasswordEmail(user)
-        return res.status(200).json({ message: 'Password reset request proccessed successfully. Check your email.' })
-    } catch (error) {
-        return res.status(500).json({ errors: { error: error.toString() }, message: 'Oops, something happen bad while proccessing your requset.' })
-    }
-});
-
 router.get('/send-reset-password-token-to-phone/:identifier', async (req, res) => {
     const { identifier } = req.params;
 
@@ -155,14 +142,34 @@ router.get('/send-reset-password-token-to-phone/:identifier', async (req, res) =
     }
 });
 
+router.get('/send-reset-password-email/:identifier', async (req, res) => {
+    const { identifier } = req.params;
 
-router.post('/new-password-email/:token', async (req, res) => {
+    try {
+        let user = await getUserByIdentifier(identifier);
+        if (!user) { return res.status(404).json({ errors: { message: 'User with such email/username doesn\'t exist in system.' } }); }
+        await sendResetPasswordEmail(user)
+        return res.status(200).json({ message: 'Password reset request proccessed successfully. Check your email.' })
+    } catch (error) {
+        return res.status(500).json({ errors: { error: error.toString() }, message: 'Oops, something happen bad while proccessing your requset.' })
+    }
+});
+
+router.get('/resetPassword/:identifier', async (req, res) => {
+    const { identifier } = req.params;
+
+    res.render('resetPassword', { title: 'Reset-Password', identifier })
+});
+
+router.post('/new-password-email', async (req, res) => {
     const { token } = req.params;
-
+    debugger
     try {
         const { errors, isValid } = resetPassword(req.body)
         if (!isValid) { return res.status(500).json({ errors }) }
+
         let { password } = req.body
+
         let decodedToken = await decodToken(token, process.env.RESET_PASSWORD_SECRET)
         let { id } = decodedToken;
 
